33355b014b450d078b9c9f6f2d974959317614f7
mohammadreza-khakpour <rezakhakpoorboraz@gmail.com>
2021-04-25 20:27:50 -0800

    addlibrarieswithbooks-method-extracted

--- DatabaseConnectedLibraryApp/Program.cs
+++ DatabaseConnectedLibraryApp/Program.cs
@@ -1,20 +1,31 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace DatabaseConnectedLibraryApp
 {
     class Program
     {
+        static LibraryDbContext db = new LibraryDbContext();
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            LibraryDbContext db = new LibraryDbContext();
-            Library library = new Library() {Address="feleke moalem" };
-            db.Libraries.Add(library);
-            Book book = new Book() {Title="Armageddon", Library=library};
-            db.Books.Add(book);
-            db.SaveChanges();
+
             Console.WriteLine("Bye Bye World!\n\t... press Enter to exit ...");
             Console.ReadKey();
+        }
+        static void AddLibrariesWithBooks()
+        {
+            
+            Library library = new Library() { Address = "feleke moalem" };
+            db.Libraries.Add(library);
+            Library library2 = new Library() { Address = "feleke atlasi" };
+            db.Libraries.Add(library2);
+            Book book = new Book() { Title = "Armageddon", Library = library };
+            db.Books.Add(book);
+            Book book2 = new Book() { Title = "life of pi", Library = library2 };
+            db.Books.Add(book2);
+            db.SaveChanges();
         }
     }
 }
