b1fc39a47335f3f2e6f7acaff470bcaedd71bbc8
mohammadreza-khakpour <rezakhakpoorboraz@gmail.com>
2021-04-25 23:49:53 -0800

    in-book-class-virtual-keyword-added

--- DatabaseConnectedLibraryApp/Book.cs
+++ DatabaseConnectedLibraryApp/Book.cs
@@ -9,7 +9,7 @@ namespace DatabaseConnectedLibraryApp
         public int Id { get; set; }
         public string Title { get; set; }
         public int LibraryId { get; set; }
-        public Library Library { get; set; }
+        public virtual Library Library { get; set; }
 
     }
 }
